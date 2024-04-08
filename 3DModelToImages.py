25050a9ec7a10eaf953975a0d482f5b0b340942d
Jonatandlr <A01735993@tec.mx>
2024-04-07 23:05:59 -0600

    add feature to object complex

--- 3DModelToImages.py
+++ 3DModelToImages.py
@@ -6,12 +6,22 @@ from rembg import remove
 
 
 
-def creationMesh(fileNameMesh:str):
+def creationMesh(fileNameMesh,fileNameTexture):
     # Load the mesh
-    mesh=o3d.io.read_triangle_mesh('./3DModelsObjects/cocacolaVieja/cocaCola.glb',True)
+    meshArray=[]
+    for i in range(len(fileNameMesh)):
+        
+        data=[]
+        data.append(o3d.io.read_triangle_mesh(fileNameMesh[i],True))
+        material = o3d.visualization.rendering.MaterialRecord()
+        material.albedo_img = o3d.io.read_image(fileNameTexture[i])
+        data.append(material)
+        meshArray.append(data)
+        
+   
 
     # Get the axis aligned bounding box
-    bbox = mesh.get_axis_aligned_bounding_box()
+    bbox = meshArray[0][0].get_axis_aligned_bounding_box()
     bbox.color = (1, 0, 0)
     center = bbox.get_center()
 
@@ -28,30 +38,31 @@ def creationMesh(fileNameMesh:str):
             random_number = np.random.uniform(0, np.pi/3)
             matrix = [[random_number],[0],[0]]
             
-        R = mesh.get_rotation_matrix_from_axis_angle(matrix)
-        mesh.rotate(R,center)
-       
+        R = meshArray[0][0].get_rotation_matrix_from_axis_angle(matrix)
+        for mesh in meshArray:
+            mesh[0].rotate(R,center)
     
-    return mesh 
+    return meshArray
 
 
 def creationWindow(mesh,name,output):
     # Create a window
     vis = o3d.visualization.O3DVisualizer(name, 1280, 720)
     vis.set_background((0., 0.78, 0., 1.0), None)
-    vis.add_geometry("objeto",mesh)
+    for i in range(len(mesh)):
+        vis.add_geometry(f"mesh_{i}",mesh[i][0],mesh[i][1])
     vis.reset_camera_to_default()
     vis.animation_time_step = 1.0
     vis.show_skybox(False)
 
-    # Define arguments for some objects that are not output with reset_camera_to_defaul
-    arg0 = 0.1
-    arg1 = np.array([0.0, 0.0, 0.0], dtype=np.float32).reshape(3, 1)
-    arg2 = np.array([0.0, 0.0, 0.2], dtype=np.float32).reshape(3, 1)
-    arg3 = np.array([0.0, 1.0,0.0], dtype=np.float32).reshape(3, 1)
+    # # Define arguments for some objects that are not output with reset_camera_to_defaul
+    # arg0 = 0.1
+    # arg1 = np.array([0.0, 0.0, 0.0], dtype=np.float32).reshape(3, 1)
+    # arg2 = np.array([0.0, 0.0, 0.2], dtype=np.float32).reshape(3, 1)
+    # arg3 = np.array([0.0, 1.0,0.0], dtype=np.float32).reshape(3, 1)
 
-    # Call the function with the defined arguments
-    vis.setup_camera(arg0, arg1, arg2, arg3)
+    # # Call the function with the defined arguments
+    # vis.setup_camera(arg0, arg1, arg2, arg3)
 
     o3d.visualization.gui.Application.instance.add_window(vis)
     vis.export_current_image(output)
@@ -89,8 +100,9 @@ def creationWindow(mesh,name,output):
 meshes=[
     {
         'name':'cocaCola',
-        'mesh':'cocacolaVieja/cocaCola.glb',
-        'output':'./outputImagesModel3D/cocaCola/'
+        'mesh':['./CocaCompleja/Node0.glb','./CocaCompleja/Node0_2.glb','./CocaCompleja/Node0_3.glb','./CocaCompleja/Node0_4.glb'],
+        'textures':['./CocaCompleja/texture.png','./CocaCompleja/texture_2.png','./CocaCompleja/texture_3.png','./CocaCompleja/texture_4.png'],
+        'output':'./outputImagesModel3D/cocaColaLumaAI/'
     }
 ]
 
@@ -98,14 +110,13 @@ meshes=[
 
 if __name__ == '__main__':
     meshArray=[]
-
-    times=3
+    times=1
     # Create the meshes
     for mesh in meshes:
         for i in range(times):
-            meshArray.append(creationMesh(mesh['mesh']))
             print(f'Creating mesh {i} for {mesh["name"]}')
-
+            meshArray.append(creationMesh(mesh['mesh'],mesh['textures']))
+    print(meshArray)
     #Importante para que no se cuelgue 
     o3d.visualization.gui.Application.instance.initialize()
 
